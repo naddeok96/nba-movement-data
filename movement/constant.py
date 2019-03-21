c34582a02ec051aeca4ca35ca7fbd922464fd077
naddeok96 <48805713+naddeok96@users.noreply.github.com>
2019-03-21 13:06:28 -0700

    Edited to Personal Path
    changed this data_dir for personal path

--- movement/constant.py
+++ movement/constant.py
@@ -1,6 +1,6 @@
 import os
 # change this data_dir for personal path
-if os.environ['HOME'] == '/home/neil':
-    data_dir = '/home/neil/projects/nba-movement-data'
+if os.environ['HOME'] == 'C:/Users/Kyle Naddeo/Desktop/':
+    data_dir = 'C:/Users/Kyle Naddeo/Desktop/roboballer/nba-movement-data'
 else:
     raise Exception("Unspecified data_dir, unknown environment")

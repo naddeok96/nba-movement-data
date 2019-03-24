31ac628a05a88874b5984f11e17ff5bae8d264f2
naddeok96 <48805713+naddeok96@users.noreply.github.com>
2019-03-24 14:45:03 -0700

    explicitly state directory

--- movement/constant.py
+++ movement/constant.py
@@ -1,7 +1,2 @@
-import os.path
-home_folder = os.path.expanduser('~')
-# change this data_dir for personal path
-if home_folder == 'E:/':
+
     data_dir = 'E:/Roboballer/nba-movement-data'
-else:
-    raise Exception("Unspecified data_dir, unknown environment")

47dc9f0ddb8534dda5f140efdbcce1ca672e97e1
naddeok96 <48805713+naddeok96@users.noreply.github.com>
2019-03-24 14:36:00 -0700

    changed 'HOME' to windows friendly expanduser

--- movement/constant.py
+++ movement/constant.py
@@ -1,6 +1,7 @@
-import os
+import os.path
+home_folder = os.path.expanduser('~')
 # change this data_dir for personal path
-if os.environ['HOME'] == 'E:/':
+if home_folder == 'E:/':
     data_dir = 'E:/Roboballer/nba-movement-data'
 else:
     raise Exception("Unspecified data_dir, unknown environment")
